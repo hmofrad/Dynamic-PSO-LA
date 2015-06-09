5517477766cbf295f5b6cbd385e7fb42924708be
Mohammad Hasanzadeh-Mofrad <m.hasanzadeh.mofrad@gmail.com>
2015-06-09 18:33:11 +0430

    Delete checkbnd.m

--- checkbnd.m
+++
@@ -1,9 +0,0 @@
-function tf = checkbnd(x,lbound,ubound)
-    tf = true;
-    for k=1:length(x)
-        if(tf && x(k)>=lbound && x(k)<=ubound)
-            tf = true;
-        else
-            tf = false;
-        end
-    end
